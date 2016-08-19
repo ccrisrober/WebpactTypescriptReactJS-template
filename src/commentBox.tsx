29a83aae127af47993759e6d4a60655a1bddb5b4
Cristian Rodríguez <ccrisrober@gmail.com>
2016-08-19 18:27:23 +0200

    TODO: Convert commentBox to ts style

---
+++ src/commentBox.tsx
@@ -0,0 +1,87 @@
+// TODO: http://staxmanade.com/2015/08/playing-with-typescript-and-jsx/
+
+import * as React from "react";
+
+var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+    alert("ENVIADO");
+    // TODO: send request to the server
+    this.setState({author: '', text: ''});
+  },
+  render: function() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+export default CommentBox;
--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,25 @@ import * as _ from "lodash";
 import * as $ from "jquery";
 
 import Hello from "./Hello";
+//import CommentBox from "./CommentBox"
+import CommentBox from "./CommentBox";
 
 ReactDOM.render(
 	<Hello name="Willson" data="[1, 2, 3, 4]" />,
 	document.getElementById("root")
+);
+
+
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"},
+  {id: 3, author: "Luke Skywalker", text: "I´m not your children ..."}
+];
+
+
+ReactDOM.render(
+	<CommentBox data={data}/>,
+	document.getElementById("root2")
 );
 
 $(() => {
